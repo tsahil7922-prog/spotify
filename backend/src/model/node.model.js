ab679070fc80b140004e33e109b497048882170b
Sahil Thakur <sthakur@corbus.com>
2026-06-10 01:14:41 +0530

    latest code

--- backend/src/model/node.model.js
+++ backend/src/model/node.model.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const nodeSchema = new mongoose.Schema({
     title:String,
     description:String,
-    age:Number,
+   
 
 })
 
 // modal will helps todo CRUD operations on the database(Tough without it)
 const nodeModel = mongoose.model("node", nodeSchema);
-model.exports = nodeModel;
+module.exports = nodeModel;
