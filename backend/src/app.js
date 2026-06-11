2bf2d9ff9fba4cccc5207f3e6789dfae3960c8ff
Shaii7922 <thakur.st.11@pg.com>
2026-06-12 01:48:44 +0530

    chnages backend done

--- backend/src/app.js
+++ backend/src/app.js
@@ -26,4 +26,12 @@ app.post("/create-post", upload.single("image"), async (req, res) => {
   return res.status(201).json({ message: "Post created succesfully", post });
 });
 
+
+
+
+app.get("/posts", async (req, res)=>{
+  const posts = await postModel.find()
+  return res.status(200).json({message: "Posts fetched successfully", posts})
+})
+
 module.exports = app;
